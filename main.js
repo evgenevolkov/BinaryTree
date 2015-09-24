b24baf21cccc84cc13f6577c8b3ea457141d28d0
EvgeneVolkov <evgenevolkov@gmail.com>
2015-09-24 15:58:04 +0300

    solution 1

--- main.js
+++ main.js
@@ -2,4 +2,41 @@
  * Created by Jenya on 24.09.15.
  */
 
-console.log ("begin");
+function getTreeValue (tree) {
+    var treeValue=[];
+    treeValue.push(tree.value);
+    if (tree.isLeaf()) {
+        return treeValue }
+    if (tree.left) {treeValue.push(getTreeValue(tree.left))}
+    if (tree.right) {treeValue.push(getTreeValue(tree.right))}
+    return treeValue;
+}
+
+sampleTree = {
+    left: {
+        left: null,
+        right: {
+            left: null,
+            right: null,
+            value: 2,
+            isLeaf: function(){return true}
+        },
+        value: 4,
+        isLeaf: function(){return false}
+    },
+    right: {
+        left: null,
+        right: {
+            left: null,
+            right: null,
+            value: 4,
+            isLeaf: function(){return true}
+        },
+        value: 4,
+        isLeaf: function(){return false}
+    },
+    value: 4,
+    isLeaf: function(){return false}
+};
+
+console.log("sampleTree is:" + getTreeValue(sampleTree));
